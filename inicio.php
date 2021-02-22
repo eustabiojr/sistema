3fa36e44cbb5c08e03289df41574fb51fca7cbd0
eustabiojr <eustabiojr@gmail.com>
2021-02-21 21:27:43 -0300

    Saíndo para pegar Nildes

--- inicio.php
+++ inicio.php
@@ -7,7 +7,28 @@
     <title>Sistema</title>
 </head>
     <h2>Meu novo sistema</h2>
-    <p><?php echo "<p>O retorno é: " . $_GET['var'] . "</p>\n"; ?></p>
+<?php
+$url = $_SERVER['HTTP_HOST'];
+$uri = $_SERVER['REQUEST_URI'];
+
+# $u = "http" . (isset($_SERVER['HTTP_HOST']))
+
+if (isset($_SERVER['HTTPS'])) {
+    echo "<p>É HTTPS</p>\n\r";
+} else {
+    echo "<p>Não é HTTPS</p>\n\r"; 
+}
+echo "<p> A hospedagem é: " . $url . "</p>\r\n";
+echo "<p> A URI é: " . $uri . "</p>\r\n";
+?>
+    <p><?php echo "<p>A chave é: " . $_GET['p'] . "</p>\n"; ?></p>
+     <!-- <p><?php echo "<p>O valor é: " . $_GET['v'] . "</p>\n"; ?></p> -->
+    <div>
+        <ul>
+            <li><a href="inicio.php?p=1&v=casa">Enviar Param 1</a></li>
+            <!-- <li><a href="inicio.php?p=2&v=apartamento">Enviar Param 2</a></li> -->
+        </ul>
+    </div>
     
 <body>
     

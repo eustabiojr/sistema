9f7653225df604b980785e97bb33c8ff0bb1805c
eustabiojr <eustabiojr@gmail.com>
2021-03-31 12:46:26 -0300

    Recipiente Navs em andamento.

--- Bib/Estrutura/Bugigangas/Base/Recipiente/Navs.php
+++ Bib/Estrutura/Bugigangas/Base/Recipiente/Navs.php
@@ -9,10 +9,46 @@
  # Espaço de nomes
 namespace Estrutura\Bugigangas\Base\Recipiente;
 
+use Estrutura\Bugigangas\Base\Elemento;
+
 /**
  * Classe Navs 
  */
-class Navs 
+class Navs extends Elemento 
 {
+    public function __construct($param = NULL)
+    {
+        parent::__construct('ul');
 
+        $this->class = 'nav';
+
+        if (is_array($param) AND isset($param['nome_link'])) {
+            $nome_link = $param['nome_link'];
+        } else {
+            $nome_link = $param;
+        }
+
+        if (isset($param['sub_classe'])) {
+            $this->class = 'card ' . $param['sub_classe'];
+        }
+
+        $link = new Elemento('a');
+        $link->{'class'} = 'nav-link';
+        $link->href = '#';
+        $link->adic($nome_link);
+
+        $nav_item = new Elemento('li');
+        $nav_item->{'class'} = 'nav-item';
+        $nav_item->adic($link);
+        #parent::adic($cabecalho);
+    }
+
+    /**
+     * Método adic
+     */
+    public function adic($conteudo)
+    {
+        $this->corpo->adic($conteudo);
+        parent::adic($this->corpo);
+    }
 }
